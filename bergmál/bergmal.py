0222f137be44eabeeb7f765c31a90e566adb3eb0
sigurdurhaukur <56935611+sigurdurhaukur@users.noreply.github.com>
2022-09-17 15:44:08 +0200

    some changes

--- bergmál/bergmal.py
+++ bergmál/bergmal.py
@@ -1,4 +1,4 @@
 # https://iceland.kattis.com/problems/iceland.bergmal
 
-line = str(input()) # the line of code that Bergur sends
+line = str(input()) # the line of code that Bergur sends    
 print(line)
