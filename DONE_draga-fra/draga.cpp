9973fe62ad56d5bd95a9fac7e5ffc72a274eeaa9
sigurdurhaukur <56935611+sigurdurhaukur@users.noreply.github.com>
2024-09-25 17:39:09 +0200

    solved 1 prob

---
+++ DONE_draga-fra/draga.cpp
@@ -0,0 +1,15 @@
+// https://open.kattis.com/problems/dragafra
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int n;
+  int m;
+
+  cin >> n;
+  cin >> m;
+
+  cout << n - m << endl;
+}
